561c19bd52939908917cd19dd4ade26c32f8014a
Augusto Cs <augustocsuarez1985@gmail.com>
2024-04-07 09:40:27 +1000

    link caal action

--- src/components/CallAction/CallAction.jsx
+++ src/components/CallAction/CallAction.jsx
@@ -8,7 +8,9 @@ const CallAction = () => {
           <div className="col-lg-8 col-md-10" style={{ textAlign: 'center' }}>
             <h2 className="mt-3 font-weight-bold mainFontBold">¿Necesitas vender en internet?</h2>
             <p className="mb-4 mainFontRegular">A lo largo de varios años, hemos tenido el privilegio de colaborar con diversas empresas, potenciando sus ventas en mercados tan diversos como Colombia, Estados Unidos, España y Australia. Nuestra fórmula infalible para garantizar ventas consistentes en el competitivo mundo digital se basa en una comunicación estratégica y efectiva a través de plataformas líderes como Facebook, Instagram y Google. Nos enorgullece poner a tu alcance todo nuestro conocimiento y experiencia para impulsar el crecimiento de tu negocio. ¡Permítenos ser tu socio de éxito!</p>
-            <button className='btn btn-lg btn-primary shadow' style={{color:'#FFF',fontSize:'18px',fontWeight:'bold',backgroundColor:'#4ABCC9',borderRadius:'11px',padding:'11px'}}>Trabaj&eacute;mos Juntos</button>
+            <a href="#contact">
+              <button className='btn btn-lg btn-primary shadow' style={{ color: '#FFF', fontSize: '18px', fontWeight: 'bold', backgroundColor: '#4ABCC9', borderRadius: '11px', padding: '11px' }}>Trabaj&eacute;mos Juntos</button>
+            </a>
           </div>
         </div>
       </div>
